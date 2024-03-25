6b1b636e36c1230d2fe5f7cee8738154e642c0eb
TheMalachite <eliasgheeraert@gmail.com>
2024-03-25 16:08:07 +0100

    TP5: Ajout d'un sous-programme qui renvoie l'opposer d'un entier

--- Program.cs
+++ Program.cs
@@ -10,13 +10,20 @@ bool supeq_ou_pas(int a, int b) {
     }
 }
 
+int opposer_de_z(int z) {
+    return -z;
+}
+
 int x = 2;
 int y = 3;
 int a = 6;
 int b = 3;
+int z = -7;
 int resultat = somme(x,y);
 bool resultat_supeg_ou_pas = supeq_ou_pas(a,b);
+int opposer = opposer_de_z(z);
 
 Console.WriteLine($"La somme de {x} et {y} est {resultat}");
 Console.WriteLine(resultat_supeg_ou_pas ? $"{a} est superieur ou egal à {b}" : $"{a} n'est superieur ou egal à {b}");
+Console.WriteLine($"L'opposer de {z} est {opposer}");
 
