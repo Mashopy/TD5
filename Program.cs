991767c5c256edc0e212a6f15893a6b1ccbb0457
TheMalachite <eliasgheeraert@gmail.com>
2024-03-25 16:12:37 +0100

    TP5: Ajout d'un sous-programme de moyenne entre deux entier

--- Program.cs
+++ Program.cs
@@ -14,6 +14,10 @@ int opposer_de_z(int z) {
     return -z;
 }
 
+float moyenne_entier(int a, int b) {
+    return ((float)a+(float)b)/2;
+}
+
 int x = 2;
 int y = 3;
 int a = 6;
@@ -22,8 +26,9 @@ int z = -7;
 int resultat = somme(x,y);
 bool resultat_supeg_ou_pas = supeq_ou_pas(a,b);
 int opposer = opposer_de_z(z);
+float moyenne = moyenne_entier(a,b);
 
 Console.WriteLine($"La somme de {x} et {y} est {resultat}");
 Console.WriteLine(resultat_supeg_ou_pas ? $"{a} est superieur ou egal à {b}" : $"{a} n'est superieur ou egal à {b}");
 Console.WriteLine($"L'opposer de {z} est {opposer}");
-
+Console.WriteLine($"La moyenne de {a} et {b} est {moyenne}");
