76cdcaf3c76b342b6b1c2d338684177e7f10005e
TheMalachite <eliasgheeraert@gmail.com>
2024-03-25 16:01:10 +0100

    TP5: Ajout d'un sous-programme de comparaison d'entier

--- Program.cs
+++ Program.cs
@@ -2,8 +2,21 @@
     return x + y;
 }
 
+bool supeq_ou_pas(int a, int b) {
+    if (a >= b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int x = 2;
 int y = 3;
+int a = 6;
+int b = 3;
 int resultat = somme(x,y);
+bool resultat_supeg_ou_pas = supeq_ou_pas(a,b);
 
 Console.WriteLine($"La somme de {x} et {y} est {resultat}");
+Console.WriteLine(resultat_supeg_ou_pas ? $"{a} est superieur ou egal à {b}" : $"{a} n'est superieur ou egal à {b}");
+
